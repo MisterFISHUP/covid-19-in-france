1f7cbb13eeac82220ec9c65275fe0df924e7eb4e
FISH UP <MisterFISHUP@users.noreply.github.com>
2021-03-11 23:24:05 +0100

    add some configs

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,33 +1,75 @@
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
 module.exports = {
-  title: 'My Site',
-  tagline: 'The tagline of my site',
-  url: 'https://your-docusaurus-test-site.com',
+  title: '法國 COVID-19 日誌',
+  tagline: '一本記錄法國新冠肺炎 (Covid-19) 的中文日誌。',
+  url: 'https://covid-fr.misterfishup.com',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'Mister FISH UP', // Usually your GitHub org/user name.
+  projectName: 'covid-19-in-france', // Usually your repo name.
+  i18n: {
+    defaultLocale: 'zh-Hant',
+    locales: ['zh-Hant'],
+    localeConfigs: {
+      'zh-Hant': {
+        label: '正體中文',
+      },
+    },
+  },
   themeConfig: {
+    hideableSidebar: true,
+    // algolia: {
+    //   apiKey: '',
+    //   indexName: '',
+    // },
+    // image: "", // Relative to your site's "static" directory.
+    // googleAnalytics: {
+    //   trackingID: 'UA-x-x',
+    //   anonymizeIP: false,
+    // },
+    colorMode: {
+      defaultMode: 'light',
+      respectPrefersColorScheme: true,
+      switchConfig: {
+        darkIcon: '🌙',
+      }
+    },
+    // announcementBar: {
+    //   id: 'welcom_to_new_site', // Any value that will identify this message.
+    //   content:
+    //     'We are looking to revamp our docs, please fill <a target="_blank" rel="noopener noreferrer" href="#">this survey</a>',
+    //   backgroundColor: '#fafbfc', // Defaults to `#fff`.
+    //   textColor: '#091E42', // Defaults to `#000`.
+    // },
+    prism: {
+      theme: require('prism-react-renderer/themes/github'),
+      darkTheme: require('prism-react-renderer/themes/dracula'),
+    },
     navbar: {
-      title: 'My Site',
+      hideOnScroll: true,
+      // style: 'primary', // or 'dark'
+      title: '法國 COVID-19 日誌',
       logo: {
         alt: 'My Site Logo',
         src: 'img/logo.svg',
+        srcDark: 'img/undraw_docusaurus_mountain.svg',
       },
       items: [
         {
-          to: 'docs/',
-          activeBasePath: 'docs',
-          label: 'Docs',
+          to: 'journal/',
+          activeBasePath: 'journal',
+          label: '日誌',
           position: 'left',
         },
-        {to: 'blog', label: 'Blog', position: 'left'},
+        // { to: 'blog', label: 'Blog', position: 'left' },
         {
           href: 'https://github.com/facebook/docusaurus',
-          label: 'GitHub',
+          // label: 'GitHub',
           position: 'right',
+          className: 'header-github-link',
+          'aria-label': 'GitHub repository',
         },
       ],
     },
@@ -87,15 +129,14 @@ module.exports = {
       {
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/',
+          routeBasePath: 'journal',
+          // editUrl:
+          //   'https://github.com/facebook/docusaurus/edit/master/website/',
         },
         blog: {
           showReadingTime: true,
-          // Please change this to your repo.
-          editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+          // editUrl:
+          //   'https://github.com/facebook/docusaurus/edit/master/website/blog/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.scss'),
