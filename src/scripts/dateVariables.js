d8a1442200849b84b17aa832732765a636d97d42
FISH UP <MisterFISHUP@users.noreply.github.com>
2021-04-24 23:52:33 +0200

    set lastest digest date = 4/23

--- src/scripts/dateVariables.js
+++ src/scripts/dateVariables.js
@@ -1,7 +1,7 @@
 const fmtMD = (n) => (n < 10 ? `0${n}` : `${n}`);
 
 export const newsEarliestDate = { m: 10, d: 26 };
-export const digestLatestDate2021 = { m: 4, d: 20 };
+export const digestLatestDate2021 = { m: 4, d: 23 };
 
 export const newsEarliestDateISO = `2020-${fmtMD(newsEarliestDate.m)}-${fmtMD(newsEarliestDate.d)}`;
 export const digestLatestDate2021ISO = `2021-${fmtMD(digestLatestDate2021.m)}-${fmtMD(digestLatestDate2021.d)}`;
